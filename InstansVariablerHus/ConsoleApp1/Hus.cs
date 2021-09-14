2b3ce18712c36b232e842bd07fb277767718f476
DNIIBOY <daniel.nettelfield@gmail.com>
2021-09-14 13:50:39 +0200

    gorm gang!

---
+++ InstansVariablerHus/ConsoleApp1/Hus.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Hus
+    {
+        private int m2;
+        private int byggeÅr;
+        private Adresse adressen = new Adresse();
+
+        public void setM2(int m2)
+        {
+            if (m2 < 0)
+            {
+                this.m2 = m2;
+            }
+
+        }
+        public int getM2()
+        {
+            return m2;
+        }
+
+        public void setAdresse(Adresse minAdresse)
+        {
+            if (minAdresse !=null)
+            {
+                this.adressen = minAdresse;
+            }
+        }
+
+        public void printInfoOmHus()
+        {
+          
+            Console.WriteLine("Størrelsen på mit hus er på: " + m2 + " og adressen er: " + adressen.getVej() + " " + adressen.getNummer() );
+        }
+
+    }
+}
