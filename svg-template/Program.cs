68bb0e2cb19fdac1aa08707c58f8ea15b7b9a7c9
DNIIBOY <daniel.nettelfield@gmail.com>
2021-11-09 12:41:45 +0100

    add svg-template

---
+++ svg-template/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace svg_template
+{
+    class Artist
+    {
+        public string style = "stroke=\"black\" stroke-width=\"1px\" fill=\"#00fffb\" opacity=\"0.3\"";
+
+        private string draw_svg(int width, int height, string shapes)
+        {
+            /*
+            Puts svg header and footer around some svg shapes.
+            Input:
+            -width: int, desired width of the svg drawing in pixels
+           - height: int, desired height of the svg drawing in pixels
+           - shapes: string, svg code describing the contents of the drawing
+            Output:
+            -string, the given svg code surrounded by header and footer tags
+            */
+            string viewbox = "viewBox=\"0 0 " + width.ToString() + " " + height.ToString() + "\"";
+            string xmlns = "xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" ";
+            string header = "<svg width=\"" + width.ToString() + "\" " + xmlns + viewbox + ">";
+            string footer = "</svg>";
+            return header + shapes + footer;
+        }
+
+        private string draw_circle(int x, int y, double radius)
+        {
+            /*
+             Creates svg code for drawing a circle.
+            Input:
+                -x: int, x-coordinate of the center of the circle in pixels
+                - y: int, y-coordinate of the center of the circle in pixels
+                - radius: int, radius of the circle in pixels
+            Output:
+                -str, svg code describing a circle
+            */
+            string cx = "cx=\"" + Convert.ToString(x) + "\"";
+            string cy = "cy=\"" + Convert.ToString(y) + "\"";
+            string rad = "r=\"" + Convert.ToString(radius) + "\"";
+            return "<circle " + cx + " " + cy + " " + rad + " " + style + "/>";
+        }
+
+        private void save(string svg, string filename)
+        {
+            using (StreamWriter sw = new StreamWriter(filename))
+            {
+                sw.WriteLine(svg);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Artist leo = new Artist();
+            string shapes = "";
+            shapes = shapes + leo.draw_circle(220, 220, 200);
+            shapes = shapes + leo.draw_circle(220, 220, 180);
+
+            string drawing = leo.draw_svg(500, 500, shapes);
+            leo.save(drawing, @"test.svg");
+
+        }
+    }
+}
