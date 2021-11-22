b26ed3e614b049d98bdb656e351d2c55f42681be
DNIIBOY <daniel.nettelfield@gmail.com>
2021-11-22 08:47:53 +0100

    add calender csv thing

---
+++ Calendar/Aftaler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Dato
+{
+    public class Aftaler
+    {
+        public string Load(DateTime date)
+        {
+            String path = @"aftaler.csv";
+            String aftaler = "";
+
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.GetEncoding("iso-8859-1")))
+            {
+                String s = "";
+                aftaler += sr.ReadLine();
+
+                while ((s = sr.ReadLine()) != null)
+                {
+                    aftaler += "\n" + s;
+                }
+            }
+            return aftaler;
+        }
+
+        public void Save(DateTime date)
+        {
+            //Din kode skal stå her.
+        }
+    }
+}
---
+++ Calendar/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dato
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public DateTime date = new DateTime(2021, 11, 22);
+        public Aftaler aftaler = new Aftaler();
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyDate.Content = date.ToShortDateString();
+        }
+
+        private void Button_Click_next(object sender, RoutedEventArgs e)
+        {
+            liste_af_aftaler.Text = "Ingen aftaler";
+            date = date.AddDays(1);
+            MyDate.Content = date.ToShortDateString();
+            liste_af_aftaler.Text = aftaler.Load(date);
+        }
+
+        private void Button_Click_prev(object sender, RoutedEventArgs e)
+        {
+            liste_af_aftaler.Text = "Ingen aftaler";
+            date = date.AddDays(-1);
+            MyDate.Content = date.ToShortDateString();
+            liste_af_aftaler.Text = aftaler.Load(date);
+        }
+
+        private void Button_Click_save(object sender, RoutedEventArgs e)
+        {
+            aftaler.Save(date);
+        }
+
+        private void Button_Click_load(object sender, RoutedEventArgs e)
+        {
+            aftaler.Load(date);
+        }
+    }
+}
