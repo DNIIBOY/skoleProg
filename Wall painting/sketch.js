48c28ac5ecbf607d11cedd2e7e675a25dad328ea
DNIIBOY <daniel@nettelfield.com>
2022-03-25 14:59:56 +0100

    pong

--- Wall painting/sketch.js
+++ Wall painting/sketch.js
@@ -1,63 +1,102 @@
+let circleX = 300
+let circleY = 120
+let xSpeed = 8;
+let ySpeed = 8;
+
+let p1Pos = 300;
+let p2Pos = 300;
+
+let p1Score = 0;
+let p2Score = 0;
+
+let circleSize = 60;
+
+document.addEventListener('keydown', function(e) {
+    switch (e.keyCode) {
+        case 38:
+			// up
+			if (p2Pos > 0){
+				p2Pos -= 10;
+			}
+            break;
+        case 40:
+			// down
+			if (p2Pos+200 < height){
+				p2Pos += 10;
+			}
+            break;
+        case 87:
+			// w
+			if (p1Pos > 0){
+				p1Pos -= 10;
+			}
+            break;
+        case 83:
+			// s
+			if (p1Pos+200 < height){
+				p1Pos += 10;
+			}
+            break;
+    }
+});
+
 function setup(){
-    createCanvas(500, 500);
-    background(220);
-    // Center gridlines
-    line(width/2, 0, width/2, height);
-    line(0, height/2, width, height/2);
-    // Borders
-    stroke(0, 128, 255);
-    strokeWeight(4);
-    line(2, 2, width, 2)
-    line(2, height-2, width, height-2);
-    line(2, 2, 2, height-2);
-    line(width-2, 2, width-2, height-2);
+    createCanvas(windowWidth, windowHeight);
 }
 
+
 function draw(){
-    // Hortizontal lines
-    stroke(0, 128, 255);
+    background(0);
+
+    stroke(255);
     strokeWeight(4);
-    line(20, 30, 200, 30);
-    line(20, 150, 200, 150);
+    // line(2, 2, width, 2)
+    // line(2, height-2, width, height-2);
+	
+    // line(2, 2, 2, height-2);
+    // line(width-2, 2, width-2, height-2);
+	ellipse(circleX, circleY, circleSize);
 
-    // Vertical lines
-    for(let i = 1; i<=10; i++){
-        line(20*i, 30, 20*i, 150);
-    }
+	circleX += xSpeed;
+	circleY += ySpeed;
 
-    // Circles
-    noStroke();
-    fill(255, 128, 0);
-    let circleGrid = 5;  // Size of circle grid
-    for(let i=0; i<circleGrid; i++){
-        for(let j=0; j<circleGrid; j++){
-            if (i%2==0){
-                circle(40+i*40, 280+j*40, 20);
-            }
-            else{
-                circle(40+i*40, 300+j*40, 20);
-            }
-        }
-    }
+	if (((circleX + circleSize/2 >= width-50) && p2Pos <= circleY && circleY <= p2Pos + 200)){
+		xSpeed = -xSpeed;
+		circleSize += 1
+	}
 
-    // Squares
-    stroke(0, 128, 255);
-    let squareGrid = 5;  // Size of square grid
-    for(let i=0; i<squareGrid; i++){
-        for(let j=0; j<squareGrid; j++){
-            if (i%2==0){
-                square(280+i*40, 20+j*40, 20);
-            }
-            else{
-                square(280+i*40, 45+j*40, 20);
-            }
-        }
-    }
+	if (((circleX - circleSize/2 <= 80) && p1Pos <= circleY && circleY <= p1Pos + 200)){
+		xSpeed = -xSpeed;
+		circleSize += 1
+	}
 
-    // Diagonal lines
-    stroke(255, 0, 255);
-    strokeWeight(2);
-    for(let i=0; i<5; i++){
-        line(350-i*20, 300+i*20, 450-20*i, 400+20*i);
-    }
+	if (circleY + circleSize/2 >= height || circleY - circleSize/2 <= 0){
+		ySpeed = -ySpeed;
+		circleSize += 1
+	}
+
+
+
+	if (circleX > width){
+		p1Score += 1
+		circleX = 300
+		circleY = 120
+		xSpeed = 8;
+		ySpeed = 8;
+	}
+
+	if (circleX < 0){
+		p2Score += 1
+		circleX = 300
+		circleY = 120
+		xSpeed = 8;
+		ySpeed = 8;
+	}
+
+	textSize(32)
+	text(p1Score, width/2-50, 50)
+	text(p2Score, width/2+50, 50)
+
+	rect(50, p1Pos, 30, 200)
+	rect(width-50, p2Pos, 30, 200)
 }
---
+++ Wall painting/sketch1.js
@@ -0,0 +1,63 @@
+function setup(){
+    createCanvas(500, 500);
+    background(220);
+    // Center gridlines
+    line(width/2, 0, width/2, height);
+    line(0, height/2, width, height/2);
+    // Borders
+    stroke(0, 128, 255);
+    strokeWeight(4);
+    line(2, 2, width, 2)
+    line(2, height-2, width, height-2);
+    line(2, 2, 2, height-2);
+    line(width-2, 2, width-2, height-2);
+}
+
+function draw(){
+    // Hortizontal lines
+    stroke(0, 128, 255);
+    strokeWeight(4);
+    line(20, 30, 200, 30);
+    line(20, 150, 200, 150);
+
+    // Vertical lines
+    for(let i = 1; i<=10; i++){
+        line(20*i, 30, 20*i, 150);
+    }
+
+    // Circles
+    noStroke();
+    fill(255, 128, 0);
+    let circleGrid = 5;  // Size of circle grid
+    for(let i=0; i<circleGrid; i++){
+        for(let j=0; j<circleGrid; j++){
+            if (i%2==0){
+                circle(40+i*40, 280+j*40, 20);
+            }
+            else{
+                circle(40+i*40, 300+j*40, 20);
+            }
+        }
+    }
+
+    // Squares
+    stroke(0, 128, 255);
+    let squareGrid = 5;  // Size of square grid
+    for(let i=0; i<squareGrid; i++){
+        for(let j=0; j<squareGrid; j++){
+            if (i%2==0){
+                square(280+i*40, 20+j*40, 20);
+            }
+            else{
+                square(280+i*40, 45+j*40, 20);
+            }
+        }
+    }
+
+    // Diagonal lines
+    stroke(255, 0, 255);
+    strokeWeight(2);
+    for(let i=0; i<5; i++){
+        line(350-i*20, 300+i*20, 450-20*i, 400+20*i);
+    }
+}
