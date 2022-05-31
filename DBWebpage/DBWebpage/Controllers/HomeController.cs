750386d23467a87d1b62b09d604c2b0d0b50c3e5
DNIIBOY <daniel@nettelfield.com>
2022-05-31 09:42:21 +0200

    getting pretty close ngl

--- DBWebpage/DBWebpage/Controllers/HomeController.cs
+++ DBWebpage/DBWebpage/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DBWebpage.Models;
 
 namespace DBWebpage.Controllers
 {
@@ -12,6 +13,12 @@ namespace DBWebpage.Controllers
         {
             List < Person > personList = new List<Person>();
             List < Forening > foreningList = new List<Forening>();
+
+
+            using (ForeningEntities entity = new ForeningEntities())
+            {
+
+            }
             return View();
         }
 
