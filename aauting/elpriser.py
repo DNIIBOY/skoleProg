0f823940066a50d1edaceeead53c2af584c9e224
DNIIBOY <daniel@nettelfield.com>
2022-10-27 13:42:35 +0200

    electricity price with random hours

--- aauting/elpriser.py
+++ aauting/elpriser.py
@@ -10,7 +10,7 @@ for point in data:
         clean_data.append(point)
 
 
-def find_cheapest_consecutive(hour_count: int, price_data: dict):
+def find_cheapest_consecutive(hour_count: int, price_data: dict) -> set[int]:
     cheapest_price = -1
     cheapest_time = -1
     for i in range(len(price_data) - hour_count):
@@ -24,7 +24,32 @@ def find_cheapest_consecutive(hour_count: int, price_data: dict):
     return cheapest_price, cheapest_time
 
 
-if __name__ == "__main__":
-    cheapest_price, cheapest_time = find_cheapest_consecutive(2, clean_data)
+def find_cheapest_random(hour_count: int, price_data: dict) -> list[dict]:
+    sorted_prices = sorted(price_data, key=lambda d: d["SpotPriceDKK"])  # Sort by price
+    return sorted_prices[:hour_count]  # Return cheapest
+
+
+def parse_date(date: str) -> str:
+    date = date[:-6]  # Remove min and sec
+    date = date.replace("T", "  Kl: ")  # Clean time shown
+    return date
+
+
+def consec():
+    hour_count = int(input("How many consecutive hours to be on?: "))
+    cheapest_price, cheapest_time = find_cheapest_consecutive(hour_count, clean_data)
     cheapest_time = clean_data[cheapest_time]["HourDK"]
-    print(f"Cheapest time : {cheapest_time}\nCheapest price: {cheapest_price}")
+    print(f"Cheapest time : {parse_date(cheapest_time)}\nCheapest price: {cheapest_price}")
+
+
+def random():
+    hour_count = int(input("How many random hours to be on?: "))
+    cheapest_prices = find_cheapest_random(hour_count, clean_data)
+    print("---Cheapest Hours---")
+    for p in cheapest_prices:
+        print(parse_date(p["HourDK"]))
+
+
+if __name__ == "__main__":
+    random()
+
