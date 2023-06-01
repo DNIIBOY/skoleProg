3a910269a9d03c1370a53a20b788420b7f30f9db
DNIIBOY <daniel@nettelfield.com>
2023-06-01 11:30:44 +0200

    add exam thing

---
+++ eksamen/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace eksamen
+{
+    public class Person {
+      public int id { get; set; }
+      public string name { get; set; }
+      public int age { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          var p1 = new Person();
+          p1.id = 1;
+          p1.name = "Grom";
+          p1.age = 12;
+
+
+          var p2 = new Person();
+          p2.id = 2;
+          p2.name = "Mandekage";
+          p2.age = 19;
+
+          var p3 = new Person();
+          p3.id = 3;
+          p3.name = "Gåstav";
+          p3.age = 99;
+
+          Person[] folk = {
+            p1,
+            p2,
+            p3
+          };
+          foreach (var person in folk){
+            if (person.age > 14){
+              Console.WriteLine(person.name);
+            }
+          }
+        }
+    }
+}
